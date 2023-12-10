0684fe717211cb390c8e98ff32fa2f5a0ae2922d
ddilan2 <ddilan2@illinois.edu>
2023-12-10 16:06:13 -0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# TODO: Not implemented
+def find_max_base_pairs(seq):
+    
+    return 0
+
+# TODO: Not implemented
+def find_secondary_structure(seq):
+    return 0
+    
+#TODO: Hairpin loops with minimum length l
+
+#TODO: Make test file
