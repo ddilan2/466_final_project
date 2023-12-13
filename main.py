442f4b4e07640dc406cf0f81f3616d97985de36b
Diwakar Basak <diwakarbasak@gmail.com>
2023-12-13 15:03:03 -0600

    Added backtrace

--- main.py
+++ main.py
@@ -15,10 +15,7 @@ def find_max_base_pairs(seq):
             if j - i >= 0:
                 down = dp[i + 1][j]
                 left = dp[i][j - 1]
-                a = 0
-                if (seq[i], seq[j]) in pairs:
-                    a = 1
-                diag = dp[i + 1][j - 1] + a
+                diag = dp[i + 1][j - 1] + int((seq[i], seq[j]) in pairs)
                 rc = max([dp[i][t] + dp[t + 1][j] for t in range(i, j)], default=0)
             
                 dp[i, j] = max(down, left, diag, rc)
@@ -27,14 +24,42 @@ def find_max_base_pairs(seq):
     return dp
 
 # TODO: Not implemented
-def find_secondary_structure(seq):
-    subseq_max_base_pairs = find_max_base_pairs(seq)
-    #Find backtrace of dp matrix
 
-    return 0
+
+
+
+def find_secondary_structure(seq):
+    dp = find_max_base_pairs(seq)
+    #Find backtrace of dp matrix
+    optimal_base_pairings = []
+    def backtrace(dp, seq, i, j):
+        if j - i >= 0:
+            if dp[i][j] == dp[i + 1][j]:
+                backtrace(dp, seq, i + 1, j)
+            elif dp[i][j] == dp[i][j - 1]:
+                backtrace(dp, seq, i, j - 1)
+            elif dp[i][j] == dp[i + 1][j - 1] + int((seq[i], seq[j]) in pairs):
+                optimal_base_pairings.append((i, j))
+                backtrace(dp, seq, i + 1, j - 1)
+            else:
+                for k in range(i + 1, j - 1):
+                    if dp[i][j] == dp[i][k] + dp[k + 1][j]:
+                        backtrace(dp, seq, i, k)
+                        backtrace(dp, seq, k + 1, j) 
+                        break
+    L = len(seq)
+    backtrace(dp, seq, 0, L - 1) 
+    output_list = ['.' for _ in range(L)]
+    for pair in optimal_base_pairings:
+        (i, j) = pair
+        output_list[i] = '('
+        output_list[j] = ')'
+    output_str = ''.join(output_list)
+    print('Optimal Base Pair: ' + output_str)
+    return output_str
     
 #TODO: Hairpin loops with minimum length l
 
 #TODO: Make test file
-
-find_max_base_pairs("GGGAAAUCC")
+seq = "GGGAAAUCC"
+find_secondary_structure(seq)
